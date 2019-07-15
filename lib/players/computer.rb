12d6e954b40d667e6c564a1e21db9ff27fdbf825
Anthony Graniero <ananfro@gmail.com>
2019-07-15 04:18:22 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,14 @@
 module Players
   class Computer < Player
     def move(board)
-      board(rand(1..9))
+      move = nil 
+      
+      if !board.taken?(5)
+        move = '5'
+      elsif board.turn_count ==1 
+        move = '1'
+      elsif board.turn_count ==2
+        move = [1,3,7,9].detect {|index| !boardta
     end
     
   end
