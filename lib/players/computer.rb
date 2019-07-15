05a80997cfe29936f41d98ffaf0a065a60bd0169
Anthony Graniero <ananfro@gmail.com>
2019-07-15 04:32:19 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -15,7 +15,9 @@ module Players
         Game::WIN_COMBINATIONS.detect do |combo|
           if combo.select {|i| board.position(i+1) == token}.size == 2 && combo.any?{|i| board.position(i+1) == " "}
             move = combo.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
-          elsif combo
+          elsif combo.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && combo.any?{|i| board.position(i+1) == " "}
+            move = combo.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+          end
     end
     
   end
